ed8611a782364930f5b7ccc3df8825ae6efa2320
Emil Kjelsrud <emilkje@gmail.com>
2012-03-08 08:59:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+	<title>SelectText Plugin</title>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+	<script type="text/javascript" src="select.js"></script>
+	<script type="text/javascript">
+		$(function(){
+			$('#arbitrary').click(function(){
+				//Click handler initiated
+				$(this).selectText();
+			});
+		});
+	</script>
+</head>
+<body>
+	<span id="arbitrary">Click this text to select it.</span>
+</body>
+</html>
